6adb4de70957340ca2404c570e721ca9c9587e0c
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-05-17 22:21:06 +0300

    Login.kt is added and activity_login.xml will be designed with relative layout

---
+++ app/src/main/java/com/example/personalhealthtracker/Login.kt
@@ -0,0 +1,11 @@
+package com.example.personalhealthtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
