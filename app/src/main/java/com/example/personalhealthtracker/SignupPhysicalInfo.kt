8e4c22644ff39fc04d7b3046745934b589c90844
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-05-29 19:00:14 +0300

    ViewBinding settings applied into SignupPhysicalInfo.kt

--- app/src/main/java/com/example/personalhealthtracker/SignupPhysicalInfo.kt
+++ app/src/main/java/com/example/personalhealthtracker/SignupPhysicalInfo.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.personalhealthtracker.databinding.FragmentSignupPhysicalInfoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +14,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [fragment_signup_physical_info.newInstance] factory method to
+ * Use the [SignupPhysicalInfo.newInstance] factory method to
  * create an instance of this fragment.
  */
-class fragment_signup_physical_info : Fragment() {
+class SignupPhysicalInfo : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
+
+    private var _binding : FragmentSignupPhysicalInfoBinding?= null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +40,15 @@ class fragment_signup_physical_info : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_physical_info, container, false)
+       _binding = FragmentSignupPhysicalInfoBinding.inflate(inflater,container,false)
+
+      return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
@@ -44,12 +58,12 @@ class fragment_signup_physical_info : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_signup_physical_info.
+         * @return A new instance of fragment FragmentSignupPhysicalInfo.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            fragment_signup_physical_info().apply {
+            SignupPhysicalInfo().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
