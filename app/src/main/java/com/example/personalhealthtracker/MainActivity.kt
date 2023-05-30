ed9c6e365ce5a22952ea74a8289d3ebf0218e924
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-05-31 01:01:40 +0300

    activity_main.xml and MainActivity.kt files are cleaned up for better architecture

--- app/src/main/java/com/example/personalhealthtracker/MainActivity.kt
+++ app/src/main/java/com/example/personalhealthtracker/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    private lateinit var signupButton: Button
-    private lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
@@ -20,40 +18,10 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        signupButton = binding.buttonSignup
-        loginButton = binding.buttonLogin
-
-        signupButton.setOnClickListener {
-            openSignupPage()
-        }
-
-        loginButton.setOnClickListener {
-            openLoginPage()
-        }
 
 
     }
 
 
-    fun openSignupPage(){
-        //işlem yapabilmek için fragment manager çağırdık
-        val fragmentManager = supportFragmentManager
-        // fragment managarden fragmentTranscation nesnesi oluşturduk ki işlem yapabilelim
-        val fragmentTransaction = fragmentManager.beginTransaction()
 
-        // ilk fragmenti aldık
-        val firstFragment = SignupUserInfo()
-
-
-        fragmentTransaction.replace(com.google.android.material.R.id.container,firstFragment).commit()
-
-    }
-
-    fun openLoginPage(){
-        val intent = Intent(this,Login::class.java)
-        startActivity(intent)
-
-        // eğer geri dönsün istemezsen...
-//        finish()
-    }
 }
