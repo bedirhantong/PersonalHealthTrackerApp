853a3e81a3a6af967982ee451b5f20ffe59df922
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-03 01:38:28 +0300

    Controlling if the device is running on Android Q or not with the giving permissions

---
+++ app/src/main/java/com/example/personalhealthtracker/other/TrackingUtility.kt
@@ -0,0 +1,24 @@
+package com.example.personalhealthtracker.other
+
+import android.content.Context
+import android.os.Build
+import com.example.personalhealthtracker.Manifest
+import pub.devrel.easypermissions.EasyPermissions
+
+object TrackingUtility {
+    fun hasLocationPermissions(context:Context)=
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.hasPermissions(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }else{
+            EasyPermissions.hasPermissions(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+}
