76c0d72e6983ef77b0a1cb30605d12c58d9ed8eb
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-02 23:37:32 +0300

    making unvisible the bottomNavigationBar is applied on the TrackRunningFragment

--- app/src/main/java/com/example/personalhealthtracker/TrackRunningFragment.kt
+++ app/src/main/java/com/example/personalhealthtracker/TrackRunningFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.personalhealthtracker.databinding.FragmentTrackRunningBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class TrackRunningFragment : Fragment() {
-
+    private var _binding: FragmentTrackRunningBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +22,29 @@ class TrackRunningFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track_running, container, false)
+        _binding = FragmentTrackRunningBinding.inflate(inflater,container,false)
+
+        val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
+        navigationBar?.visibility = View.GONE
+
+
+
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+
+        val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
+        navigationBar?.visibility = View.VISIBLE
+
+        _binding = null
+    }
 }
