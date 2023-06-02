04f1515d1b909dde1fbd1d0ead77562862095b66
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-03 00:45:32 +0300

    making unvisible the bottomNavBar on the StartNewActivityFragment

--- app/src/main/java/com/example/personalhealthtracker/StartNewActivityFragment.kt
+++ app/src/main/java/com/example/personalhealthtracker/StartNewActivityFragment.kt
@@ -8,11 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.personalhealthtracker.databinding.FragmentStartNewActivityBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class StartNewActivityFragment : Fragment() {
 
-
+    private lateinit var chosenOne:String
     private var _binding : FragmentStartNewActivityBinding?= null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,38 +30,73 @@ class StartNewActivityFragment : Fragment() {
         _binding = FragmentStartNewActivityBinding.inflate(inflater,container,false)
         val view: View = binding.root
 
-        binding.runningView.setOnClickListener {
-            binding.runningView.setImageResource(R.drawable.running_icon_selected)
-        }
-        binding.hikingView.setOnClickListener{
-            binding.runningView.setImageResource(R.drawable.hiking_icon_selected)
-        }
-        binding.cyclingView.setOnClickListener{
-            binding.runningView.setImageResource(R.drawable.cycling_icon_selected)
-        }
-        binding.divingView.setOnClickListener{
-            binding.runningView.setImageResource(R.drawable.diving_icon_selected)
-        }
+        val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
+        navigationBar?.visibility = View.GONE
 
-
-
+        changeChosenOne()
 
 
 
         binding.StartButton.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_startNewActivityFragment2_to_trackRunningFragment)
+            if (chosenOne == "running"){
+                Navigation.findNavController(it).navigate(R.id.action_startNewActivityFragment2_to_trackRunningFragment)
+            }
+            else
+                Toast.makeText(requireContext(),"Please choose running activity because that is the " +
+                        "only feature exist",Toast.LENGTH_LONG).show()
         }
+
         binding.cancelButton.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_startNewActivityFragment2_to_profileFragment2)
             Toast.makeText(requireContext(),"You canceled the starting the new activity",Toast.LENGTH_SHORT).show()
-
         }
-
 
         return view
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
+        navigationBar?.visibility = View.VISIBLE
     }
+
+
+    private fun changeChosenOne(){
+        binding.runningView.setOnClickListener {
+//            binding.divingView.setBackgroundColor(resources.getColor(R.color.black))
+//            if(chosenOne != "running"){
+//                binding.runningView.setImageResource(R.drawable.running_icon_selected)
+            chosenOne ="running"
+//            }
+//            else
+//                binding.runningView.setImageResource(R.drawable.running_nonselected_icon)
+        }
+
+        binding.hikingView.setOnClickListener{
+//            if (chosenOne != "hiking"){
+//                binding.hikingView.setImageResource(R.drawable.hiking_icon_selected)
+            chosenOne ="hiking"
+//            }else
+//                binding.hikingView.setImageResource(R.drawable.hiking_nonselected_icon)
+
+        }
+
+        binding.cyclingView.setOnClickListener{
+//            if (chosenOne != "cycling"){
+//                binding.cyclingView.setImageResource(R.drawable.cycling_icon_selected)
+            chosenOne ="cycling"
+//            } else
+//                binding.cyclingView.setImageResource(R.drawable.cycling_nonselected_icon)
+        }
+
+        binding.divingView.setOnClickListener{
+//            if (chosenOne != "diving"){
+//                binding.divingView.setImageResource(R.drawable.diving_icon_selected)
+            chosenOne ="diving"
+//            }else
+//                binding.divingView.setImageResource(R.drawable.diving_nonselected_icon)
+        }
+    }
+
+
 }
