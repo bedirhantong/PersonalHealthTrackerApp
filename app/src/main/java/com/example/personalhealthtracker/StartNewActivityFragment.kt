55fc8a88265f54fac53c8f2db6009daf2b5999f6
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-02 21:06:23 +0300

    jpg images are replaced with png ones

--- app/src/main/java/com/example/personalhealthtracker/StartNewActivityFragment.kt
+++ app/src/main/java/com/example/personalhealthtracker/StartNewActivityFragment.kt
@@ -1,11 +1,13 @@
 package com.example.personalhealthtracker
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.personalhealthtracker.databinding.FragmentProfileBinding
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.personalhealthtracker.databinding.FragmentStartNewActivityBinding
 
 class StartNewActivityFragment : Fragment() {
@@ -25,8 +27,32 @@ class StartNewActivityFragment : Fragment() {
         _binding = FragmentStartNewActivityBinding.inflate(inflater,container,false)
         val view: View = binding.root
 
+        binding.runningView.setOnClickListener {
+            binding.runningView.setImageResource(R.drawable.running_icon_selected)
+        }
+        binding.hikingView.setOnClickListener{
+            binding.runningView.setImageResource(R.drawable.hiking_icon_selected)
+        }
+        binding.cyclingView.setOnClickListener{
+            binding.runningView.setImageResource(R.drawable.cycling_icon_selected)
+        }
+        binding.divingView.setOnClickListener{
+            binding.runningView.setImageResource(R.drawable.diving_icon_selected)
+        }
 
 
+
+
+
+
+        binding.StartButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_startNewActivityFragment2_to_trackRunningFragment)
+        }
+        binding.cancelButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_startNewActivityFragment2_to_profileFragment2)
+            Toast.makeText(requireContext(),"You canceled the starting the new activity",Toast.LENGTH_SHORT).show()
+
+        }
 
 
         return view
