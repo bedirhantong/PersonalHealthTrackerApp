6b4dd408408ef6061de33cd35753cb357af46299
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-03 23:11:03 +0300

    Notification channel id and name is defined

--- app/src/main/java/com/example/personalhealthtracker/other/Constants.kt
+++ app/src/main/java/com/example/personalhealthtracker/other/Constants.kt
@@ -6,4 +6,10 @@ object Constants {
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+    const val ACTION_SHOW_TRACK_RUNNING_FRAGMENT = "ACTION_SHOW_TRACK_RUNNING_FRAGMENT"
+
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    // at least NOTIFICATION_ID should be 1 or a higher value
+    const val NOTIFICATION_ID = 1
 }
