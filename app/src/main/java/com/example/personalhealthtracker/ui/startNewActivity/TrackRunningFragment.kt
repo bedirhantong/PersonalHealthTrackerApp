921cd5802cb7723f7c1461b4654b040f80206128
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-03 04:20:41 +0300

    GoogleMaps API key is applied into the project so that we can track live

--- app/src/main/java/com/example/personalhealthtracker/ui/startNewActivity/TrackRunningFragment.kt
+++ app/src/main/java/com/example/personalhealthtracker/ui/startNewActivity/TrackRunningFragment.kt
@@ -9,12 +9,18 @@ import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.personalhealthtracker.R
 import com.example.personalhealthtracker.databinding.FragmentTrackRunningBinding
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class TrackRunningFragment : Fragment() {
     private var _binding: FragmentTrackRunningBinding? = null
     private val binding get() = _binding!!
+
+    private var map: GoogleMap? = null
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,31 +45,68 @@ class TrackRunningFragment : Fragment() {
         val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
         navigationBar?.visibility = View.GONE
 
-        binding.drawBackButton.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_trackRunningFragment_to_startNewActivityFragment2)
-            Toast.makeText(requireContext(),"You canceled the activity you have just been doing", Toast.LENGTH_SHORT).show()
+        binding.mapView.onCreate(savedInstanceState)
+
+        //this will load our map
+        binding.mapView.getMapAsync {
+            map = it
+            /*
+            If you do not worry about life cycle of mapView when you use it in a blank fragment then
+            your app will crash here
+             */
         }
+//        binding.drawBackButton.setOnClickListener {
+//            Navigation.findNavController(it).navigate(R.id.action_trackRunningFragment_to_startNewActivityFragment2)
+//            Toast.makeText(requireContext(),"You canceled the activity you have just been doing", Toast.LENGTH_SHORT).show()
+//        }
 
     }
 
+
+
     override fun onStop() {
         super.onStop()
+        binding.mapView.onStop()
         val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
         navigationBar?.visibility = View.GONE
     }
 
     override fun onResume() {
         super.onResume()
-
+        binding.mapView.onResume()
         val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
         navigationBar?.visibility = View.GONE
+    }
 
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+//        binding.mapView?.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // this will help us to cache our map
+        binding.mapView.onSaveInstanceState(outState)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         val navigationBar = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView2)
         navigationBar?.visibility = View.VISIBLE
+
+//        binding.mapView.onDestroy()
+//        binding.mapView?.onDestroy()
 
 
         _binding = null
