0954dd6637903dfc00d6fc2dd7fc43ae2f36c0ab
Bedirhan Tong <20200808033@ogr.akdeniz.edu.tr>
2023-06-05 23:00:46 +0300

    Navigation between different Activities are implemented

---
+++ app/src/main/java/com/example/personalhealthtracker/ui/startNewActivity/DivingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.personalhealthtracker.ui.startNewActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.personalhealthtracker.R
+
+class DivingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diving)
+    }
+}
